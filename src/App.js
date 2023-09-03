08c0212b03ef3d31e854a46cc25d9c18ccdc9cbd
Kostiantyn Lapin <lapin.kostiantyn@gmail.com>
2023-09-03 12:54:40 +0200

    Move data and function to App Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 import React from 'react'
 import NoteSpace from "./NoteSpace"
+import { useEffect, useState } from 'react'
+import "./NoteSpace.css"
+import CreateNote from './CreateNote';
 const App = () => {
+  const [isFirstRender,SetFirstRender] = useState(true);
+  const [notes,setNotes] = useState([]);
+  useEffect(() => {
+   
+  
+		const savedNotes = JSON.parse(
+			window.localStorage.getItem('react-notes-app-data')
+		);
+    
+		if (savedNotes) {
+     
+			setNotes(savedNotes);
+		}
+	}, []);
+
+	useEffect(() => {
+    if(isFirstRender){
+      SetFirstRender(false);
+      return;
+    }
+  
+		window.localStorage.setItem(
+			'react-notes-app-data',
+			JSON.stringify(notes)
+
+		);
+	}, [notes]);
+
+  const addNote = (newNote)=>{
+    
+    setNotes([newNote,...notes]);
+  }
+  const editNote = (newNote)=>{
+    let newNotes = [...notes];
+    let noteIndex = newNotes.findIndex((element)=> element.id ==newNote.id);
+    console.log(`noteIndex ${noteIndex}`);
+    newNotes[noteIndex] = newNote;
+    setNotes(newNotes);
+  }
+  const deleteNote = (id)=>{
+    let newNotes = notes.filter((note)=> note.id != id);
+    setNotes(newNotes);
+  }
+   
+ 
   return (
-    <div>
-      <NoteSpace/>
-    </div>
+    <main>
+      <CreateNote addNote={addNote}/>
+      <NoteSpace notes={notes} editNote={editNote} deleteNote={deleteNote}/>
+    </main>
   )
 }
 
--- src/NoteSpace.js
+++ src/NoteSpace.js
@@ -1,63 +1,20 @@
-import React, { useEffect, useState } from 'react'
+
 import "./NoteSpace.css"
 import Note from './Note'
-import CreateNote from './CreateNote';
-const NoteSpace = () => {
-  const [isFirstRender,SetFirstRender] = useState(true);
-  const [notes,setNotes] = useState([]);
-  useEffect(() => {
-   
-  
-		const savedNotes = JSON.parse(
-			window.localStorage.getItem('react-notes-app-data')
-		);
-console.log(savedNotes);
-		if (savedNotes) {
-     
-			setNotes(savedNotes);
-		}
-	}, []);
 
-	useEffect(() => {
-    if(isFirstRender){
-      SetFirstRender(false);
-      return;
-    }
-  
-		window.localStorage.setItem(
-			'react-notes-app-data',
-			JSON.stringify(notes)
-
-		);
-	}, [notes]);
-
-  const addNote = (newNote)=>{
-    
-    setNotes([newNote,...notes]);
-  }
-  const editNote = (newNote)=>{
-    let newNotes = [...notes];
-    let noteIndex = newNotes.findIndex((element)=> element.id ==newNote.id);
-    console.log(`noteIndex ${noteIndex}`);
-    newNotes[noteIndex] = newNote;
-    setNotes(newNotes);
-  }
-  const deleteNote = (id)=>{
-    let newNotes = notes.filter((note)=> note.id != id);
-    setNotes(newNotes);
-  }
+const NoteSpace = ({notes,editNote,deleteNote}) => {
+ 
    
   console.log(notes);
   return (
-    <div>
-    <CreateNote addNote={addNote}/>
+   
     <div className='noteSpace'> 
     {
     
       notes.map((note)=><Note data= {note} edit = {editNote} deleteNote ={deleteNote}/>)
      }
     </div>
-    </div>
+    
   )
 }
 
